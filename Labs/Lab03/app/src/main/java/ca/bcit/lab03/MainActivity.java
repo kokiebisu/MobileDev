1c3f9c5bc5d1a5d75f96f45c0e219969118abcdf
kokiebisu <kenichikona@gmail.com>
2019-09-15 15:45:17 -0700

    completed api version button

--- Labs/Lab03/app/src/main/java/ca/bcit/lab03/MainActivity.java
+++ Labs/Lab03/app/src/main/java/ca/bcit/lab03/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -28,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
     public void onSpeakActivity(View view) {
         Intent intent = new Intent(this, SpeakActivity.class);
         startActivity(intent);
+    }
+
+    public void onCheckVersion(View view) {
+        String manufacturer = android.os.Build.MANUFACTURER;
+        String model = android.os.Build.MODEL;
+        int version = Build.VERSION.SDK_INT;
+        String versionRelease = Build.VERSION.RELEASE;
+
+        String messageText = "manufacturer " + manufacturer
+                + " \n model " + model
+                + " \n version " + version
+                + " \n versionRelease " + versionRelease;
+        Toast toast = Toast.makeText(this, messageText, Toast.LENGTH_SHORT);
+        toast.show();
+
     }
 
 }
